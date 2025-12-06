ab6f53b738cbfa6db9c0707900b6de0478982149
alom1209 <alamfahim496@gmail.comm>
2025-12-06 15:25:22 +0600

    successfully connected to mongodb

---
+++ index.js
@@ -0,0 +1,33 @@
+const express=require("express")
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app=express()
+const cors=require("cors");
+const port=process.env.port||3000;
+// middleware
+app.use(cors())
+app.use(express.json())
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xbqvadl.mongodb.net/?appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    await client.connect();
+    const 
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+app.get('/',(req,res)=>{
+    res.send("homehero backend server is running")
+})
+app.listen(port,()=>{
+    console.log(`backend server is running on ${port}`)
+})
