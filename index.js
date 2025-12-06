e02bb9977b4f662ae19232850738e55d620de10f
alom1209 <alamfahim496@gmail.comm>
2025-12-06 21:32:47 +0600

    added the booking deleting part

--- index.js
+++ index.js
@@ -92,7 +92,13 @@ async function run() {
       const result=await cursor.toArray()
       res.send(result)
     })
-    // 
+    // for deleting booking
+    app.delete('/bookings/:id',async(req,res)=>{
+      const id=req.params.id
+      const query={_id:new ObjectId(id)}
+      const result=await bookings.deleteOne(query)
+      res.send(result)
+    })
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
